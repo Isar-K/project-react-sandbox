90b827719077197d5f5c82c7aabd8a86f4be9872
emptxd <155116181+emptxd@users.noreply.github.com>
2025-11-06 16:56:41 +0100

    Add immersive fleet map experience

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,13 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import InfoCards from './components/InfoCards'  // Add this
+import Map from './pages/Map'
 
-// TEMP pages so routes don't break
-function MapPage() { 
-  return <div style={{ padding: '2rem', color: '#0b2545' }}>Map</div> 
+function DataPage() {
+  return <div style={{ padding: '2rem', color: '#0b2545' }}>Data</div>
 }
-function DataPage() { 
-  return <div style={{ padding: '2rem', color: '#0b2545' }}>Data</div> 
-}
-function AboutPage() { 
-  return <div style={{ padding: '2rem', color: '#0b2545' }}>About</div> 
+function AboutPage() {
+  return <div style={{ padding: '2rem', color: '#0b2545' }}>About</div>
 }
 
 function Home() {
@@ -29,7 +26,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/map" element={<MapPage />} />
+        <Route path="/map" element={<Map />} />
         <Route path="/data" element={<DataPage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
